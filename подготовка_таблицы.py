ddddb6930076e933a05d90d7e7ada74f62eb5f7c
tmci_acdc <elec_tmci@ukr.net>
2020-05-27 16:03:11 +0300

    firs test

---
+++ подготовка_таблицы.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+
+name = input("File name :")
+fileName = "./" + name + '.xlsx'
+fileNameNew = "./new_" + name + '.xlsx'
+
+wb = load_workbook(fileName)
+
+sheet = wb.active
+# sheet = wb.worksheets('Арендаторы')
+
+rowVal = [[9, 29], [43, 467]]
+
+for k in range(len(rowVal)):
+    for i in range(rowVal[k][0], rowVal[k][1]):
+        oldVal = sheet.cell(row=i, column=5).value
+        sheet.cell(row=i, column=6).value = oldVal
+        sheet.cell(row=i, column=5).value = ''
+
+sheet.column_dimensions.group('H', 'AD', hidden=True)
+sheet.column_dimensions.group('A', hidden=True)
+sheet.column_dimensions.group('D', hidden=True)
+sheet.row_dimensions.group(467, 623, hidden=True)
+
+print('Ok!')
+
+wb.save(fileNameNew)
+wb.close()
